f98a3c305c2cbdc75041856a6e0221b5f8e88b18
Alejandro Narancio <pixel@Alejandros-MacBook-Pro.local>
2018-06-13 12:09:54 -0300

    adding SetupComponent, this component will contain all the UI related to the watcher setup like: ethereum url, contract address, and contract abi

---
+++ src/components/setup/SetupComponent.jsx
@@ -0,0 +1,25 @@
+import React, {Component} from "react";
+
+
+export default class SetupComponent extends Component{
+
+    render =()=>
+        <div class="col-sm-12 pt-2 pb-2 bg-secondary mb-4">
+            <div class="d-block" id="navbarSupportedContent">
+                <div class="form-group">
+                    <input id="txtConnectionUrl" type="text" class="form-control" placeholder="<URL:PORT> ex.: http://localhost:8545" aria-label="<URL:PORT> ex.: http://localhost:8545" aria-describedby="basic-addon2" value="ws://localhost:8545"></input>
+                </div>
+                <div class="form-group">
+                    <input id="txtContractAddress" type="text" class="form-control" placeholder="contract address" aria-label="contract address" aria-describedby="basic-addon2" value="0x5D7fDc3e80b59E08fee14Ae43A00fDa3B48B0A3D"></input>
+                </div>
+                <div class="form-group">
+                    <input id="txtContractAbi" type="text" class="form-control" placeholder="contract abi" aria-label="contract abi" aria-describedby="basic-addon2"></input>
+                </div>
+                <div class="text-right">
+                    <button id="btnConnect" type="button" class="btn btn-primary">Connect</button>
+                    <button id="btnDisconnect" type="button" class="btn btn-danger disabled">Disconnect</button>
+                </div>
+            </div>
+        </div>
+
+}
